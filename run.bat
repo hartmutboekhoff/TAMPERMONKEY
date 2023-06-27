e9f84a3cb5c2290500d4f32128c78e7e181c7fdf
hartmutboekhoff <hartmut.boekhoff@t-online.de>
2023-06-27 15:53:14 +0200

    documentation, global template + some minor changes

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 cls
 
 start node dev/bin/www 
-start firefox -P Funke -url http://localhost:3000/
+start firefox -P Funke -url http://localhost:3000/ -url moz-extension://a7f72f90-8547-4f95-9f88-9c4e5558f7f3/options.html#nav=dashboard
---
+++ src/template/main.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Vorlage
+// @description  UI Verbesserungen für Vorlage
+// @version      1
+// @grant        GM_getResourceURL
+// @grant        GM_getResourceText
+// @grant        GM_info
+// @grant        addStyle
+// @grant        unsafeWindow
+
+// @include      https://www.vorlage.de/*
+
+// @downloadURL          http://localhost:3000/...vorlage.../main.js
+// @updateURL            http://localhost:3000/...vorlage.../main.js
+
+// @resource  css-common http://localhost:3000/common/styles.css
+// @resource  css        http://localhost:3000/...vorlage.../styles.css
+
+// @require         http://localhost:3000/common/head.js
+// @require         http://localhost:3000/common/styles.js
+// @require         http://localhost:3000/common/KeyHandler.js
+// @require         http://localhost:3000/common/readout.js
+
+// @require         http://localhost:3000/...vorlage...vorlage.../UI.js
+
+// ==/UserScript==
+
+
+console.log(GM_info.script.name, 'Version '+GM_info.script.version, 'Initialization complete');
+console.groupEnd();
